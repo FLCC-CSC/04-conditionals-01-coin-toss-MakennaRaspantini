089efc6109c05e7f60ead9f876ed825ebabcc72f
Makenna Raspantini <mraspantini@fingerlakes.edu>
2026-03-16 00:54:31 +0000

    submission for coin_toss

--- coin_toss.py
+++ coin_toss.py
@@ -1,20 +1,20 @@
 # FILE NAME - coin_toss.py
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
+# NAME: Makenna Raspantini
+# DATE: 3/15/26
+# BRIEF DESCRIPTION: user gets heads or tails output 
 # 1. Make sure you fill out the comments above
 # 2. Write your code in the proper spot
 # 3. Be sure to answer the Reflection Questions and Attestation below
 # 4. The Sample Output has been included in this code for your convenience
 ########## ENTER YER CODE BELOW THIS LINE ##########
 
+import random
 
+print("===== Coin Flipper =====")
 
+coin = random.choice(["Tails", "Heads"])
 
-
-
-
-
+print(coin)
 
 
 ########### END YER CODE ABOVE THIS LINE ###########
@@ -44,7 +44,9 @@ Tails
 
 1. What was the hardest part of completing this lab? 
 
-
+This lab uses skills using the methods taught last chapter with "random" that was helpful 
+to utilize but the difficulty mainly came from remembering to use [] or () in their 
+appropriate spots.
 
 
 
@@ -62,6 +64,6 @@ like AI and internet sites like Chegg, CourseHero, StackOverflow, and general Go
 may impede your understanding. Please rate how well you understand the concepts in this lab: 
 [ ] I understand very little about this lab.
 [ ] I am about 50/50 on this lab; I get parts of it but not the whole picture.
-[ ] I pretty much get it.
+[x ] I pretty much get it.
 [ ] I'm solid. Totally got it.
 '''
